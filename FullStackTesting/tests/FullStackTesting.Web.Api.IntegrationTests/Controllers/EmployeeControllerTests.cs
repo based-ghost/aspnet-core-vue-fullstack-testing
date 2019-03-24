d8d245de3f8bc1b51d9e0c0d5ba186842d771bc4
Matt Areddia <mareddia@protonmail.com>
2019-03-24 13:59:27 -0500

    Re-ordering integration tests to mirror order of methods in controller (in order in class)

--- FullStackTesting/tests/FullStackTesting.Web.Api.IntegrationTests/Controllers/EmployeeControllerTests.cs
+++ FullStackTesting/tests/FullStackTesting.Web.Api.IntegrationTests/Controllers/EmployeeControllerTests.cs
@@ -54,33 +54,39 @@ namespace FullStackTesting.Web.Api.IntegrationTests.Controllers
         }
 
         [Fact]
-        public async Task CanDeleteEmployeeAsync()
+        public async Task CanAddEmployeeAsync()
         {
-            // The endpoint or route of the controller action (DeleteEmployeeAsync)
-            const int targetId = 2;
-            var httpDeleteResponse = await _client.DeleteAsync($"/api/Employee/DeleteEmployeeAsync?id={targetId}");
+            // New Employee record to be posted in content
+            var addEmployee = new Employee {
+                Id = 7,
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Department = "Claims",
+                FullTime = false
+            };
 
-            // Must be successful
-            httpDeleteResponse.EnsureSuccessStatusCode();
-
-            // The endpoint or route of the controller action (GetAllEmployeesAsync)
-            var httpResponse = await _client.GetAsync("/api/Employee/GetAllEmployeesAsync");
+            // The endpoint or route of the controller action (AddEmployeeAsync) with StringContent comprised of the employee to add
+            var addEmployeeStringContent = new StringContent(JsonConvert.SerializeObject(addEmployee), Encoding.UTF8, "application/json");
+            var httpResponse = await _client.PostAsync("/api/Employee/AddEmployeeAsync", addEmployeeStringContent);
 
             // Must be successful
             httpResponse.EnsureSuccessStatusCode();
 
-            // Deserialize and examine results (should no longer contain record having Id = 1, FirstName = 'Matt', LastName = 'Areddia')
+            // Deserialize and examine results (compare employee object returned in response to that passed in initial request - should match)
             var stringResponse = await httpResponse.Content.ReadAsStringAsync();
-            var employees = JsonConvert.DeserializeObject<List<Employee>>(stringResponse);
+            var employee = JsonConvert.DeserializeObject<Employee>(stringResponse);
 
-            Assert.DoesNotContain(employees, e => e.Id.Equals(targetId) && e.FirstName.Equals("Jane") && e.LastName.Equals("Doe"));
+            Assert.Equal(addEmployee.Id, employee.Id);
+            Assert.Equal(addEmployee.FirstName, employee.FirstName);
+            Assert.Equal(addEmployee.LastName, employee.LastName);
         }
 
         [Fact]
         public async Task CanUpdateEmployeeAsync()
         {
             // Existing Employee record to be updated
-            var updateEmployee = new Employee {
+            var updateEmployee = new Employee
+            {
                 Id = 4,
                 FirstName = "Debbie",
                 LastName = "Test",
@@ -112,31 +118,26 @@ namespace FullStackTesting.Web.Api.IntegrationTests.Controllers
         }
 
         [Fact]
-        public async Task CanAddEmployeeAsync()
+        public async Task CanDeleteEmployeeAsync()
         {
-            // New Employee record to be posted in content
-            var addEmployee = new Employee {
-                Id = 7,
-                FirstName = "TestFirstName",
-                LastName = "TestLastName",
-                Department = "Claims",
-                FullTime = false
-            };
+            // The endpoint or route of the controller action (DeleteEmployeeAsync)
+            const int targetId = 2;
+            var httpDeleteResponse = await _client.DeleteAsync($"/api/Employee/DeleteEmployeeAsync?id={targetId}");
 
-            // The endpoint or route of the controller action (AddEmployeeAsync) with StringContent comprised of the employee to add
-            var addEmployeeStringContent = new StringContent(JsonConvert.SerializeObject(addEmployee), Encoding.UTF8, "application/json");
-            var httpResponse = await _client.PostAsync("/api/Employee/AddEmployeeAsync", addEmployeeStringContent);
+            // Must be successful
+            httpDeleteResponse.EnsureSuccessStatusCode();
+
+            // The endpoint or route of the controller action (GetAllEmployeesAsync)
+            var httpResponse = await _client.GetAsync("/api/Employee/GetAllEmployeesAsync");
 
             // Must be successful
             httpResponse.EnsureSuccessStatusCode();
 
-            // Deserialize and examine results (compare employee object returned in response to that passed in initial request - should match)
+            // Deserialize and examine results (should no longer contain record having Id = 1, FirstName = 'Matt', LastName = 'Areddia')
             var stringResponse = await httpResponse.Content.ReadAsStringAsync();
-            var employee = JsonConvert.DeserializeObject<Employee>(stringResponse);
+            var employees = JsonConvert.DeserializeObject<List<Employee>>(stringResponse);
 
-            Assert.Equal(addEmployee.Id, employee.Id);
-            Assert.Equal(addEmployee.FirstName, employee.FirstName);
-            Assert.Equal(addEmployee.LastName, employee.LastName);
+            Assert.DoesNotContain(employees, e => e.Id.Equals(targetId) && e.FirstName.Equals("Jane") && e.LastName.Equals("Doe"));
         }
     }
 }
